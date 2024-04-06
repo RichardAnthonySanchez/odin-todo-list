ac9c64fa53ed016426aec7ab05f94875eaa8ddb9
richard <theirsaviorhd@gmail.com>
2024-04-06 08:53:21 -0400

    displaying default todos to the webpage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css';
 import _ from 'lodash';
 import defaultTodos from './default-todos.json'
+import viewTodos from './view-todos'
 
 function component() {
+  viewTodos(defaultTodos);
   }
   
   component();
---
+++ src/view-todos.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+
+export default function viewTodos(todos) {
+    let projectArr = [];
+    for (let i = 0; i < todos.length; i++) {
+        let eachTodo = todos[i];
+        let projectOfTodo = eachTodo.project;
+        projectArr.push(projectOfTodo);
+    }
+    let uniqueProjects = _.uniq(projectArr);
+
+    const list = document.createElement('ul');
+    for (let parseEachProject = 0; parseEachProject < uniqueProjects.length; parseEachProject++) {
+        let eachProjectContainer = document.createElement('li');
+        eachProjectContainer.innerHTML = uniqueProjects[parseEachProject];
+        list.appendChild(eachProjectContainer);
+    }
+    const body = document.querySelector('body');
+    body.appendChild(list);
+} 
