a79fb3a5c20fc56a5fae33cd8ce51e8caa6ce6ce
richard <theirsaviorhd@gmail.com>
2024-04-10 17:59:34 -0400

    added init style to the task cards

--- src/view-todos.js
+++ src/view-todos.js
@@ -25,9 +25,15 @@ export default function viewTodos(todos) {
     for (let eachFilteredTodo = 0; eachFilteredTodo < filteredTodos.length; eachFilteredTodo++) {
         let task = filteredTodos[eachFilteredTodo];
         let taskContainer = document.createElement('div');
+        let checkBox = document.createElement('input');
+        checkBox.setAttribute('type', 'checkbox');
+        checkBox.setAttribute('class', 'task-checkbox');
+        let taskContent = document.createElement('p');
+        taskContent.setAttribute('class', 'task-content');
         taskContainer.setAttribute('class', 'task');
-        taskContainer.innerHTML = task.title;
+        taskContent.innerHTML = task.title;
+        taskContainer.appendChild(checkBox);
+        taskContainer.appendChild(taskContent);
         activeTasksContainer.appendChild(taskContainer);
-    }
-        
+    }   
 } 
