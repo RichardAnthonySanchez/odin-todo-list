a533f886515a06f1dff06c9cb2b3d733e4104932
richard <theirsaviorhd@gmail.com>
2024-05-30 17:16:21 -0400

    refactored view projects method for easier readability and maintenence

--- src/view-project-title.js
+++ src/view-project-title.js
@@ -16,30 +16,62 @@ const projectViewer = (function () {
     }
 
     function viewProjects(projects) {
-        const customProjectsContainer = document.querySelector('.custom-projects');
-        customProjectsContainer.innerHTML = '';
+        const customProjectsContainer = createCustomProjectsContainer();
 
         for (let eachProject = 0; eachProject < projects.length; eachProject++) {
             let projectName = projects[eachProject].name
             
-            let navItem = document.createElement('span');
-            navItem.setAttribute('class', 'nav-item');
+            const navContainer = createNavContainer();
+            const navHeaderContainer = createNavHeaderContainer();  
+            const headerContent = createHeaderContent(projectName);
+            const menuIcon = createMenuIcon();
 
-            let navTitle = document.createElement('span');
-            navTitle.setAttribute('class', 'nav-item-left');
-        
-            let titleContent = document.createElement('h3');
-            titleContent.innerHTML = projectName;
-
-            let menuIcon = document.createElement('span');
-            menuIcon.setAttribute('class', 'material-symbols-rounded');
-            menuIcon.innerHTML = 'menu';
-            
-            navTitle.appendChild(menuIcon);
-            navTitle.appendChild(titleContent);
-            navItem.appendChild(navTitle);
-            customProjectsContainer.appendChild(navItem)
+            appendProjectElementsToNav (
+                 navHeaderContainer,
+                 navContainer, 
+                 customProjectsContainer, 
+                 menuIcon, 
+                 headerContent
+                )
         }
+    }
+
+    function createCustomProjectsContainer() {
+        const customProjectsContainer = document.querySelector('.custom-projects');
+        customProjectsContainer.innerHTML = '';
+        return customProjectsContainer;
+    }
+
+    function createNavContainer() {
+        let navContainer = document.createElement('span');
+        navContainer.setAttribute('class', 'nav-item');
+        return navContainer
+    }
+
+    function createNavHeaderContainer() {
+        let navHeaderContainer = document.createElement('span');
+        navHeaderContainer.setAttribute('class', 'nav-item-left');
+        return navHeaderContainer
+    }
+
+    function createHeaderContent(projectName) {
+        let headerContent = document.createElement('h3');
+        headerContent.innerHTML = projectName;
+        return headerContent
+    }
+
+    function createMenuIcon() {
+        let menuIcon = document.createElement('span');
+        menuIcon.setAttribute('class', 'material-symbols-rounded');
+        menuIcon.innerHTML = 'menu';
+        return menuIcon
+    }
+
+    function appendProjectElementsToNav(navHeaderContainer, navContainer, customProjectsContainer, menuIcon, headerContent) {
+        navHeaderContainer.appendChild(menuIcon);
+        navHeaderContainer.appendChild(headerContent);
+        navContainer.appendChild(navHeaderContainer);
+        customProjectsContainer.appendChild(navContainer);
     }
 
     return {
