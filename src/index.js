d93bc39cc736e8dbacd9782332204de57a4a7d19
richard <theirsaviorhd@gmail.com>
2024-07-01 17:38:05 -0400

    moved the style.css file to the styles directory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+import './styles/style.css';
 import _ from 'lodash';
 import defaultTodos from './data/default-todos.json'
 import defaultProjects from './data/default-projects.json'
