d15af2b32e3a0fc9aee5cffaaf6dc9a2f9cfb7fe
richard <theirsaviorhd@gmail.com>
2024-08-21 17:25:05 -0400

    installed eslint-config-prettier so there are no compatibility issues while formatting code.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
 
 export default [
   {
@@ -11,4 +13,5 @@ export default [
     },
   },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
